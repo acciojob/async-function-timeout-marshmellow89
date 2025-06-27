c2822b48a5f2a8268199aa48460b3d420345c74a
marshmellow89 <pritamsingh100app@gmail.com>
2025-06-28 02:58:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const mytext = document.getElementById("text").value.trim();
 const mynumber = document.getElementById("delay").value.trim();
 
 async function display(){
-	const oldtext = await new promise(resolve => setTimeout(() => resolve(mytext)));
-	const oldnumber = await new promise(resolve => setTimeout(() => resolve(mynumber)));
+	const oldtext = await new promise(resolve => setTimeout(() => resolve(mytext),1000));
+	const oldnumber = await new promise(resolve => setTimeout(() => resolve(mynumber),2000));
 	};
 document.getElementById("output").push(oldtext,oldnumber);
