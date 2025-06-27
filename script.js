156a3690c6e8b8485c42397f5508121058b1871d
marshmellow89 <pritamsingh100app@gmail.com>
2025-06-28 02:55:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const mytext = document.getElementById("text").value.trim();
+const mynumber = document.getElementById("delay").value.trim();
+
+async function display(){
+	const oldtext = await new promise(resolve => setTimeout(() => resolve(mytext)));
+	const oldnumber = await new promise(resolve => setTimeout(() => resolve(mynumber)));
+	};
+document.getElementById("output").push(oldtext,oldnumber);
