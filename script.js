1ca87594b0ed2c046bc26682133998a8556a6aec
marshmellow89 <pritamsingh100app@gmail.com>
2025-07-13 01:45:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+async function display() {
+  const mytext = document.getElementById("text").value.trim();
+  const mynumber = document.getElementById("delay").value.trim();
 
-async function display(){
-	const mytext = document.getElementById("text").value.trim();
-const mynumber = document.getElementById("delay").value.trim();
-	
-	const myoutput = await new Promise(resolve => setTimeout(() => resolve(mytext),mynumber*1000));
-	const node = document.createElement("div");
-    node.textContent = myoutput;
-	document.getElementById("output").appendChild(node);
+  const myoutput = await new Promise(resolve => 
+    setTimeout(() => resolve(mytext), mynumber * 1000)
+  );
+
+  const node = document.createElement("div");
+  node.textContent = myoutput;
+  document.getElementById("output").appendChild(node);
 }
 
-document.querySelector("form").addEventListener("submit",function(e){
-	e.preventDefault();
-	display();
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  display();
 });
