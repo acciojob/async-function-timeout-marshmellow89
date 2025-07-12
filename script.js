ecc338ca884a576f7199fe0a34c2d88fee2d8c5a
marshmellow89 <pritamsingh100app@gmail.com>
2025-07-13 01:08:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const mytext = document.getElementById("text").value.trim();
 const mynumber = document.getElementById("delay").value.trim();
 
 async function display(){
-	const oldtext = await new promise(resolve => setTimeout(() => resolve(mytext),1000));
-	const oldnumber = await new promise(resolve => setTimeout(() => resolve(mynumber),2000));
-	};
-document.getElementById("output").push(oldtext,oldnumber);
+	const myoutput = await new promise(resolve => setTimeout(() => resolve(mytext),mynumber));
+	document.getElementById("output").appendchild(myoutput);
+}
+
+document.querySelector("form").addEventListener("submit",display());
