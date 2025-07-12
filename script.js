22be6f4bebfde3dc324ec74b8204721f10074fd2
marshmellow89 <pritamsingh100app@gmail.com>
2025-07-13 01:50:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function display() {
   document.getElementById("output").appendChild(node);
 }
 
-document.querySelector("form").addEventListener("submit", function(e) {
+document.querySelector("#btn").addEventListener("submit", function(e) {
   e.preventDefault();
   display();
 });
