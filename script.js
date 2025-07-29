50c4bdf0446a2271152338c0552d881889b8e549
marshmellow89 <pritamsingh100app@gmail.com>
2025-07-29 23:39:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
+// async function display() {
+//   const mytext = document.getElementById("text").value.trim();
+//   const mynumber = document.getElementById("delay").value.trim();
+
+//   const myoutput = await new Promise(resolve => 
+//     setTimeout(() => resolve(mytext), mynumber*1000)
+//   );
+
+
+// 	const mydiv = document.getElementById("output");
+// 	mydiv.textContent = myoutput;
+// 	mydiv.appendChild(myoutput);
+// }
+
+// document.querySelector("#btn").addEventListener("click", function(e) {
+//   e.preventDefault();
+//   display();
+// });
+
+
 async function display() {
-  const mytext = document.getElementById('text').value.trim();
-  const mynumber = document.getElementById('delay').value.trim();
+  const mytext = document.getElementById("text").value.trim();
+  const mynumber = Number(document.getElementById("delay").value.trim());
 
-  const myoutput = await new Promise(resolve => 
-    setTimeout(() => resolve(mytext), mynumber * 1000)
-  );
+  // Delay using Promise
+  await new Promise(resolve => setTimeout(resolve, mynumber));
 
-
-	const mydiv = document.getElementById('output');
-	mydiv.textContent = myoutput;
-	mydiv.appendChild(mydiv);
+  // Set output text
+  const mydiv = document.getElementById("output");
+  mydiv.textContent = mytext;
 }
 
-document.querySelector("#btn").addEventListener("click", function(e) {
+document.getElementById("btn").addEventListener("click", function (e) {
   e.preventDefault();
   display();
 });
+
