0aaa4833515cda312d7181fb3dbb9fa39c9aab2c
marshmellow89 <pritamsingh100app@gmail.com>
2025-07-29 23:56:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-// async function display() {
-//   const mytext = document.getElementById("text").value.trim();
-//   const mynumber = document.getElementById("delay").value.trim();
-
-//   const myoutput = await new Promise(resolve => 
-//     setTimeout(() => resolve(mytext), mynumber*1000)
-//   );
-
-
-// 	const mydiv = document.getElementById("output");
-// 	mydiv.textContent = myoutput;
-// 	mydiv.appendChild(myoutput);
-// }
-
-// document.querySelector("#btn").addEventListener("click", function(e) {
-//   e.preventDefault();
-//   display();
-// });
-
-
 async function display() {
   const mytext = document.getElementById("text").value.trim();
-  const mynumber = Number(document.getElementById("delay").value.trim());
+  const mynumber = document.getElementById("delay").valueAsNumber();
 
-  // Delay using Promise
-  await new Promise(resolve => setTimeout(resolve, mynumber));
+  await new Promise(resolve => 
+    setTimeout(resolve, mynumber)
+  );
 
-  // Set output text
-  const mydiv = document.getElementById("output");
-  mydiv.textContent = mytext;
+
+	const mydiv = document.getElementById("output");
+	mydiv.textContent = mytext;
+	
 }
 
-document.getElementById("btn").addEventListener("click", function (e) {
+document.querySelector("#btn").addEventListener("click", function(e) {
   e.preventDefault();
   display();
 });
+
+
+// async function display() {
+//   const mytext = document.getElementById("text").value.trim();
+//   const mynumber = Number(document.getElementById("delay").value.trim());
+
+//   // Delay using Promise
+//   await new Promise(resolve => setTimeout(resolve, mynumber));
+
+//   // Set output text
+//   const mydiv = document.getElementById("output");
+//   mydiv.textContent = mytext;
+// }
+
+// document.getElementById("btn").addEventListener("click", function (e) {
+//   e.preventDefault();
+//   display();
+// });
 
